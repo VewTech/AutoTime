9ec97cc50a692215760279dc19853ecff374fe9f
Josep Sendra Serra <je.sendra@vewtech.net>
2023-02-20 15:55:24 +0100

    Added cors for the web application into the server

--- IntratimeMiddleware/Program.cs
+++ IntratimeMiddleware/Program.cs
@@ -6,13 +6,25 @@ namespace IntratimeMiddleware
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Allow cors
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  policy =>
+                                  {
+                                      policy.WithOrigins("http://autotime.azurewebsites.net");
+                                  });
+            });
 
             var app = builder.Build();
 
@@ -22,6 +34,9 @@ namespace IntratimeMiddleware
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors();
+
 
             app.UseHttpsRedirection();
 

8bd932d0b51e1308564d6a1d74c8d60a53538174
Josep Sendra Serra <je.sendra@vewtech.net>
2023-02-21 08:27:40 +0100

    Corrected class declaration error

--- Library/Schemas/Clocking.cs
+++ Library/Schemas/Clocking.cs
@@ -1,6 +1,6 @@
 ﻿namespace Library.Schemas
 {
-    public class ClockingAction
+    public class Clocking
     {
         public int INOUT_ID { get; set; }
         public int INOUT_USER_ID { get; set; }

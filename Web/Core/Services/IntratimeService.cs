4ea99b257b5b08ebf8488b6fe8a02c3b9ce2f725
Josep Sendra Serra <je.sendra@vewtech.net>
2023-02-21 11:12:19 +0100

    Added intratime middleware operations as a service

---
+++ Web/Core/Services/IntratimeService.cs
@@ -0,0 +1,21 @@
+﻿using Library;
+using Library.Schemas;
+using System.Net.Http.Json;
+
+namespace Web.Core.Services
+{
+    public class IntratimeService : IIntratimeService
+    {
+        public async Task<User> LoginUser(string user, string pin)
+        {
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("user", user),
+                new KeyValuePair<string, string>("pin", pin)
+            });
+            var response = await Clients.IntratimeMiddlewareClient.PostAsync("user/login", content);
+            if (!response.IsSuccessStatusCode) throw new HttpRequestException($"Could not login. Reason: {response.StatusCode} - {response.ReasonPhrase} - {response.Content}");
+            return await response.Content.ReadFromJsonAsync<User>();
+        }
+    }
+}
